8d718f273d7a233ace8ee5eefafd2fa79a8bcd91
Juntia <114236242+AmauryDufour@users.noreply.github.com>
2025-01-20 15:21:33 +0800

    feat: add openvino model compatibility

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ import numpy as np
 import onnx
 import onnxruntime
 from tqdm.contrib.logging import logging_redirect_tqdm
+import yaml
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -115,34 +116,44 @@ class DataManager:
         cap.release()
 
     def set_names(self, selected_model):
-        _, extension = os.path.splitext(selected_model)
-        self.model_type = extension
-        if extension == ".pt": #YOLO returns pt model names in dict format
-            class_names = YOLO(selected_model).model.names
-            if class_names:
-                self.names = YOLO(selected_model).model.names
-                logging.info("Class names loaded from pt metadata.")
-            else:
-                logging.warning("Class names not found in pt metadata.")
-                
-        elif extension == ".onnx": #ONNX model names metadata is a string
-            model = onnx.load(selected_model)
-            metadata_props = {prop.key: prop.value for prop in model.metadata_props}
-            class_names = metadata_props.get('names', None)
-            if class_names:
-                items = class_names.strip('{}').split(',')
-                self.names =  {}
-                for item in items :
-                    key, name = item.split(':')
-                    key = int(key.strip())
-                    name = name.strip().strip("'").strip('"')
-                    self.names[key] = name
-                logging.info("Class names loaded from onnx metadata.")
-            else:
-                logging.warning("Class names not found in onnx metadata.")
-                return []
+        if os.path.isdir(selected_model): #check for an openvino model if model is a folder
+            files = [f for f in os.listdir(selected_model) if os.path.isfile(os.path.join(selected_model, f))]
+            if 'metadata.yaml' in files: 
+                self.model_type = "openvino"
+                yaml_path = os.path.join(selected_model, 'metadata.yaml')
+                with open(yaml_path, 'r') as f:
+                    metadata = yaml.safe_load(f)
+                self.names = metadata.get('names', {})
+                logging.info("Class names loaded from yaml metadata.")
         else :
-            logging.warning("Unsupported model format : Class names not loaded from model metadata.")
+            _, extension = os.path.splitext(selected_model)
+            self.model_type = extension
+            if extension == ".pt": #YOLO returns pt model names in dict format
+                class_names = YOLO(selected_model).model.names
+                if class_names:
+                    self.names = YOLO(selected_model).model.names
+                    logging.info("Class names loaded from pt metadata.")
+                else:
+                    logging.warning("Class names not found in pt metadata.")
+                    
+            elif extension == ".onnx": #ONNX model names metadata is a string
+                model = onnx.load(selected_model)
+                metadata_props = {prop.key: prop.value for prop in model.metadata_props}
+                class_names = metadata_props.get('names', None)
+                if class_names:
+                    items = class_names.strip('{}').split(',')
+                    self.names =  {}
+                    for item in items :
+                        key, name = item.split(':')
+                        key = int(key.strip())
+                        name = name.strip().strip("'").strip('"')
+                        self.names[key] = name
+                    logging.info("Class names loaded from onnx metadata.")
+                else:
+                    logging.warning("Class names not found in onnx metadata.")
+                    return []
+            else :
+                logging.warning("Unsupported model format : Class names not loaded from model metadata.")
 
     def set_site_location(self, site_location):
         self.site_location = site_location
